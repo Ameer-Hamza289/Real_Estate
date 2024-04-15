cb328903fe421973201a15dd3b456785e8e844f3
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-04-15 21:46:14 +0500

    Path for controller correction

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, register } from "../controller/authController";
+import { login, logout, register } from "../controllers/authController";
 
 const router=express.Router();
 
